d71ba8e55f48096e3553a35ae8bf950016a52554
yelamanchili-murali <myel_lists@hotmail.com>
2020-12-03 07:38:37 +1100

    initial commit

---
+++ src/main/java/tbs/demos/spring/TbsDemoApplication.java
@@ -0,0 +1,13 @@
+package tbs.demos.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TbsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tbs/demos/spring/TbsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package tbs.demos.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TbsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
